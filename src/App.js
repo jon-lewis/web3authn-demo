3757d7d009344dbf69276dd488bd45268866c05d
Danny Cho <hj.danny.cho@gmail.com>
2023-02-20 17:38:11 +1300

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { createKey } from "@near-js/biometric-ed25519";
 
 function App() {
+  console.log('createKey', createKey);
+
+  useEffect(() => {
+    console.log('calling')
+    const getKey = async () => {
+      await createKey('helloworld');
+    }
+    getKey();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
